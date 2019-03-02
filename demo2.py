8fe4d4aed609233423b954b079189dff3599d9e7
UedaKyo <ueda.k.al@m.titech.ac.jp>
2019-03-02 16:41:20 +0900

    ファイルを追加

---
+++ demo2.py
@@ -0,0 +1,15 @@
+def print_b():
+    "prints b."
+    print ("b")
+
+def print_a():
+    "prints a"
+    print ("a")
+
+
+# print_b()runs on impport
+print_b()
+
+if __name__ == '__main__':
+    # print_a() is only executed when the module is run directly.
+    print_a()
---
+++ my_file.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 14:46:25 2019
+
+@author: kyoka
+"""
+
+s = 'Hello world'
+
+print(s)
